8a9c2bd6c9fd5e1482a6757f830e50ee5fe997fd
JianoFreo <jianofreo.magtangob@tup.edu.ph>
2026-03-30 21:24:31 +0800

    Add comments to clarify thread joining behavior and program termination

--- main.py
+++ main.py
@@ -19,6 +19,19 @@ thread2 = threading.Thread(target=worker, daemon=True, args=("XYZ",))
 thread1.start()
 thread2.start()
 
+# thread1.join()
+# thread2.join()
+
+# join() makes the main thread wait for a thread to finish.
+# Start both threads
+# Then pause the main program
+# Wait until:
+# thread1 finishes
+# thread2 finishes
+# Only after both are done will the program continue.
+# Think of join() as:
+#       “Main thread, don’t continue until this thread is done.”
+
 # A program will automatically terminate when only daemon threads are left running.
 # In contrast, non-daemon (user) threads must finish before the program exits.
 # daemon only runs as a background of main threads
