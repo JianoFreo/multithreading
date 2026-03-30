46d117e9e5965b55743fe9a91ac7687e44bd61d3
JianoFreo <jianofreo.magtangob@tup.edu.ph>
2026-03-30 20:57:47 +0800

    daemon thread

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ def worker():
         # if counter == 10:
         #     done = True
         
-threading.Thread(target=worker).start()
+threading.Thread(target=worker, deamon=True).start()
+
+# A program will automatically terminate when only daemon threads are left running.
+# In contrast, non-daemon (user) threads must finish before the program exits.
+# daemon only runs as a background of main threads
 
 input("Press enter to quit")
 done = True
