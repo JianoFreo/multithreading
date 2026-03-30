1f5769d291f790456cf1b88d076ecef0a19fde25
JianoFreo <jianofreo.magtangob@tup.edu.ph>
2026-03-30 21:07:27 +0800

    Refactor worker function to accept text argument and update thread initialization

--- main.py
+++ main.py
@@ -3,17 +3,21 @@ import time
 
 done = False
 
-def worker():
+def worker(text):
     # global done
     counter = 0
     while not done:
         time.sleep(1)
         counter += 1
-        print(counter)
+        print(f"{text}: {counter}")
         # if counter == 10:
         #     done = True
         
-threading.Thread(target=worker, deamon=True).start()
+thread1 = threading.Thread(target=worker, daemon=True, args=("ABC",))
+thread2 = threading.Thread(target=worker, daemon=True, args=("XYZ",))
+
+thread1.start()
+thread2.start()
 
 # A program will automatically terminate when only daemon threads are left running.
 # In contrast, non-daemon (user) threads must finish before the program exits.
